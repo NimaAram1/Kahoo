64e10bbbfc785013b3f5e759a293bf94c21f71e0
NimaAram1 <aramnima50@gmail.com>
2021-06-25 21:14:04 +0430

    course model designe(still incomplete) and make user model better(user_level added)

--- courses/apps.py
+++ courses/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class UsersConfig(AppConfig):
+class CoursesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
-    verbose_name = "کاربر"
+    name = 'courses'
+    verbose_name = "دوره"
